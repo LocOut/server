2b91fd169feac11f20ed6da580059e274f86e2ef
marvinmirtschin <marvin.mirtschin@student.hpi.uni-potsdam.de>
2015-11-27 12:15:18 +0100

    get a scale for every device

--- templates/trustlevel.js
+++ templates/trustlevel.js
@@ -1,119 +1,125 @@
-$(function () {
+function createChart(id, options){
+    $(function () {
 
-    console.log("start");
-    $('#container').highcharts({
+        console.log("start");
+        $(id).highcharts({
 
-        chart: {
-            type: 'gauge',
-            plotBackgroundColor: null,
-            plotBackgroundImage: null,
-            plotBorderWidth: 0,
-            plotShadow: false
-        },
-
-        title: {
-            text: 'Trustlevel'
-        },
-
-        pane: {
-            startAngle: -150,
-            endAngle: 150,
-            background: [{
-                backgroundColor: {
-                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-                    stops: [
-                        [0, '#FFF'],
-                        [1, '#333']
-                    ]
-                },
-                borderWidth: 0,
-                outerRadius: '109%'
-            }, {
-                backgroundColor: {
-                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
-                    stops: [
-                        [0, '#333'],
-                        [1, '#FFF']
-                    ]
-                },
-                borderWidth: 1,
-                outerRadius: '107%'
-            }, {
-                // default background
-            }, {
-                backgroundColor: '#DDD',
-                borderWidth: 0,
-                outerRadius: '105%',
-                innerRadius: '103%'
-            }]
-        },
-
-        // the value axis
-        yAxis: {
-            min: 0,
-            max: 100,
-
-            minorTickInterval: 'auto',
-            minorTickWidth: 1,
-            minorTickLength: 10,
-            minorTickPosition: 'inside',
-            minorTickColor: '#666',
-
-            tickPixelInterval: 30,
-            tickWidth: 2,
-            tickPosition: 'inside',
-            tickLength: 10,
-            tickColor: '#666',
-            labels: {
-                step: 2,
-                rotation: 'auto'
+            chart: {
+                type: 'gauge',
+                plotBackgroundColor: null,
+                plotBackgroundImage: null,
+                plotBorderWidth: 0,
+                plotShadow: false
             },
+
             title: {
-                text: '%'
+                text: 'Trustlevel'
             },
-            plotBands: [{
-                from: 0,
-                to: 40,
-                color: '#DF5353' // red
-            }, {
-                from: 40,
-                to: 80,
-                color: '#DDDF0D' // yellow
-            }, {
-                from: 80,
-                to: 100,
-                color: '#55BF3B' // green
-            }]
-        },
 
-        series: [{
-            name: 'Speed',
-            data: [80],
-            tooltip: {
-                valueSuffix: ' %'
-            }
-        }]
+            pane: {
+                startAngle: -150,
+                endAngle: 150,
+                background: [{
+                    backgroundColor: {
+                        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                        stops: [
+                            [0, '#FFF'],
+                            [1, '#333']
+                        ]
+                    },
+                    borderWidth: 0,
+                    outerRadius: '109%'
+                }, {
+                    backgroundColor: {
+                        linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                        stops: [
+                            [0, '#333'],
+                            [1, '#FFF']
+                        ]
+                    },
+                    borderWidth: 1,
+                    outerRadius: '107%'
+                }, {
+                    // default background
+                }, {
+                    backgroundColor: '#DDD',
+                    borderWidth: 0,
+                    outerRadius: '105%',
+                    innerRadius: '103%'
+                }]
+            },
 
-    },
-    // Add some life
-    function (chart) {
-        console.log("export");
-        if (!chart.renderer.forExport) {
-            setInterval(function () {
-                var point = chart.series[0].points[0],
-                    newVal,
-                    inc = Math.round((Math.random() - 0.5) * 20);
+            // the value axis
+            yAxis: {
+                min: 0,
+                max: 100,
 
-                newVal = point.y + inc;
-                if (newVal < 0 || newVal > 100) {
-                    newVal = point.y - inc;
+                minorTickInterval: 'auto',
+                minorTickWidth: 1,
+                minorTickLength: 10,
+                minorTickPosition: 'inside',
+                minorTickColor: '#666',
+
+                tickPixelInterval: 30,
+                tickWidth: 2,
+                tickPosition: 'inside',
+                tickLength: 10,
+                tickColor: '#666',
+                labels: {
+                    step: 2,
+                    rotation: 'auto'
+                },
+                title: {
+                    text: '%'
+                },
+                plotBands: [{
+                    from: 0,
+                    to: 40,
+                    color: '#DF5353' // red
+                }, {
+                    from: 40,
+                    to: 80,
+                    color: '#DDDF0D' // yellow
+                }, {
+                    from: 80,
+                    to: 100,
+                    color: '#55BF3B' // green
+                }]
+            },
+
+            series: [{
+                name: 'Speed',
+                data: options,
+                tooltip: {
+                    valueSuffix: ' %'
                 }
+            }]
 
-                point.update(newVal);
-                console.log("update");
-            }, 3000);
-        }
+        },
+        // Add some life
+        function (chart) {
+            $("text[text-anchor=end").hide();
+            //$("g.highcharts-button").hide();
+            console.log(options);
+            console.log("export");
+            if (!chart.renderer.forExport) {
+                setInterval(function () {
+                    var point = chart.series[0].points[0],
+                        newVal,
+                        inc = Math.round((Math.random() - 0.5) * 20);
+
+                    newVal = point.y + inc;
+                    if (newVal < 0 || newVal > 100) {
+                        newVal = point.y - inc;
+                    }
+
+                    point.update(newVal);
+                    console.log("update");
+                }, 3000);
+            }
+
+        });
+
+        console.log("end");
     });
-
-    console.log("end");
-});
+}
